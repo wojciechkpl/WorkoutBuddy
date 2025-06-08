ce18891fd8bc4b01228e404d2bdf873ded63001a
Wojciech Kowalinski <wojciechkowalinski@Wojciechs-MacBook-Pro-2.local>
2025-06-07 21:30:31 -0700

    Refactoring the code, connecting with antropic claude, moving keys out of config

--- backend/app/config.py
+++
@@ -1,6 +0,0 @@
-from omegaconf import OmegaConf
-import os
-
-CONFIG_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../config.yaml'))
-config = OmegaConf.load(CONFIG_PATH)
-backend_config = config.backend 
